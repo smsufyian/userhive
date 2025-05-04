449171e87119355bcce1cfeaa3e190ee80c0d7b4
muhammad.sufyian <s.m.sufyian@gmail.com>
2025-05-04 19:16:38 +0200

    Test commit

--- tests/test_app.py
+++ tests/test_app.py
@@ -29,3 +29,6 @@ def test_hello_world():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "helloworld"}
+
+def test_function():
+    assert True
